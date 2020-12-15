b8c07791e5b415b5b21027c2365cc9e7f6f3b2d3
Rama Krishna Yerramsetty <kris.nitk5@gmail.com>
2020-12-15 17:59:28 +0530

    Created a very simple app

---
+++ simpleAppUsers/src/main/java/com/techkris/simplemicroservice1/SimpleAppUsersApplication.java
@@ -0,0 +1,13 @@
+package com.techkris.simplemicroservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAppUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAppUsersApplication.class, args);
+	}
+
+}
