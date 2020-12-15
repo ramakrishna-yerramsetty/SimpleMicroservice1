2aa1267dbf0a10b496319f2864fba1f1e5f6ef71
rk <rk@rk-pc>
2020-12-15 20:40:23 +0530

    created new APIConfigServer,hardcoded to 8012 port, gave git details in application.properties created a new repository which contains application.properties which will work as centralconfig

---
+++ simpleApiConfigServer/src/main/java/com/techkris/simple/SimpleApiConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.techkris.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SimpleApiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiConfigServerApplication.class, args);
+	}
+
+}
---
+++ simpleApiConfigServer/src/test/java/com/techkris/simple/SimpleApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techkris.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
