0b5f17f960347ed5968a4769e62b14e1f51f6b06
rk <rk@rk-pc>
2020-12-15 20:06:01 +0530

    added ${PORT:0} for cli argument, tested inbuilt zuul server with users application displaying which port the application is running on Environment env

--- simpleAppUsers/src/main/java/com/techkris/simplemicroservice1/UsersController.java
+++ simpleAppUsers/src/main/java/com/techkris/simplemicroservice1/UsersController.java
@@ -1,5 +1,7 @@
 package com.techkris.simplemicroservice1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UsersController {
-
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("")
 	public String getUsers(){
-	return "hello";
+	return "working on " + env.getProperty("local.server.port");
 	}
 }
